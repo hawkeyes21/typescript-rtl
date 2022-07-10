ac5e5c3eae5ff26d18128388488dcb346dc0ee0c
Jaymin Shah <shahjaymin2101@gmail.com>
2022-07-10 14:44:58 +0530

    adds: more test cases, default values

--- src/Heading.test.tsx
+++ src/Heading.test.tsx
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import {Heading} from "./Heading";
 
-test('renders page heading', () => {
+test('default heading language is Typescript', () => {
     render(<Heading />);
-    const headingElement = screen.getByText(/React Testing Library using Typescript/i)
+    const headingElement = screen.getByText(`React Testing Library using Typescript`)
+    expect(headingElement).toBeInTheDocument();
+})
+
+test('renders page heading with language as prop', () => {
+    const lang = 'Javascript'
+    render(<Heading lang={lang}/>);
+    const headingElement = screen.getByText(`React Testing Library using ${lang}`)
     expect(headingElement).toBeInTheDocument();
 })
--- src/Heading.tsx
+++ src/Heading.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export function Heading() {
-    return <h1>React Testing Library using Typescript</h1>
+type HeadingProps = {lang?: string}
+
+export function Heading({lang = "Typescript"}: HeadingProps) {
+    return <h1>React Testing Library using {lang}</h1>
 }
