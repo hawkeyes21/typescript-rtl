b515c6ae73bcbbdff218042ed310d4fcb4f36884
Jaymin Shah <shahjaymin2101@gmail.com>
2022-07-10 12:44:10 +0530

    Passing Test for Heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+export function Heading() {
+    return <h1>React Testing Library using Typescript</h1>
+}
+
 function App() {
   return (
     <div className="App">
+        <Heading />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
